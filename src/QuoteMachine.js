0e2c8a427f2c011f77ab7e194e28f55fd19a5092
Mickael LY <mickael.ly.fr@gmail.com>
2022-03-19 11:50:37 +0100

    User Story #3

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,6 +4,9 @@ class QuoteMachine extends React.Component {
     render() {
         return <div id="quote-box">
             <div id="text">
+
+            </div>
+            <div id="author">
                 
             </div>
         </div>;
