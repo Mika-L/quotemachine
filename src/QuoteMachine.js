969490aa78a1535de59965d7d9d406821e04e845
Mickael LY <mickael.ly.fr@gmail.com>
2022-03-19 12:04:48 +0100

    User Story #6

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 class QuoteMachine extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            quote: "Mangez des pommes"
+        };
+    }
     render() {
         return <div id="quote-box">
             <div id="text">
-
+                {this.state.quote}
             </div>
             <div id="author">
-                
+
             </div>
             <button id="new-quote">Get new quote</button>
             <a id="tweet-quote" />
         </div>;
-      }
+    }
 }
 
 export default QuoteMachine;
