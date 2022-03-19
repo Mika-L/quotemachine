8cf8ed0d7ad0024acfa5e84f04101d77a19d5ef9
Mickael LY <mickael.ly.fr@gmail.com>
2022-03-19 11:19:34 +0100

    init quotemachine component

---
+++ src/QuoteMachine.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class QuoteMachine extends React.Component {
+    render() {
+        return <h1>Bonjour</h1>;
+      }
+}
+
+export default QuoteMachine;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import QuoteMachine from './QuoteMachine';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QuoteMachine />
   </React.StrictMode>,
   document.getElementById('root')
 );
