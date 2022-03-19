7e388aa1f8e91918a4a2e44207184263360f31e9
Mickael LY <mickael.ly.fr@gmail.com>
2022-03-19 12:05:54 +0100

    User Story #7

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,7 +4,8 @@ class QuoteMachine extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            quote: "Mangez des pommes"
+            quote: "Mangez des pommes",
+            author: "J.Chirac"
         };
     }
     render() {
@@ -13,7 +14,7 @@ class QuoteMachine extends React.Component {
                 {this.state.quote}
             </div>
             <div id="author">
-
+                {this.state.author}
             </div>
             <button id="new-quote">Get new quote</button>
             <a id="tweet-quote" />
